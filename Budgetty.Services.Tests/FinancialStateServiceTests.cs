015548c8b705eba9fa167c05d4b6f5bd4119d6ff
Jessica Wyatt <jessica@fourcore-computing.co.uk>
2022-12-01 01:35:13 +0000

    Added unit test for FinancialStateService

---
+++ Budgetty.Services.Tests/FinancialStateServiceTests.cs
@@ -0,0 +1,64 @@
+using Budgetty.Domain;
+using Budgetty.Domain.BudgetaryEvents;
+using Budgetty.Persistance.Repositories;
+using Budgetty.Services.Interfaces;
+using Moq;
+
+namespace Budgetty.Services.Tests
+{
+    public class FinancialStateServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task GivenUserId_WhenGetCurrentFinancialStateForUserAsyncIsCalled_ThenSnapshotAndEventsAndPoolsAreRetrievedAndUsedWithEventProcessorToGetFinancialState()
+        {
+            // Given
+            var mockFinancialSnapshotManager = new Mock<IFinancialsSnapshotManager>();
+            var mockBudgetaryRepository = new Mock<IBudgetaryRepository>();
+            var mockEventProcessor = new Mock<IEventProcessor>();
+            var mockDateTimeProvider = new Mock<IDateTimeProvider>();
+            var userId = "1234";
+            var endDate = new DateOnly(2022, 12, 1);
+            var budgetaryEvents = new List<BudgetaryEvent>();
+            var pools = new List<BudgetaryPool>();
+            var expectedFinancialState = new FinancialState(new List<BudgetaryPool>(), new List<BankAccount>(), null);
+
+            mockDateTimeProvider
+                .Setup(x => x.GetDateNow())
+                .Returns(endDate)
+                .Verifiable();
+
+            mockBudgetaryRepository
+                .Setup(x => x.GetBudgetaryEventsForUser(userId, null, endDate))
+                .Returns(budgetaryEvents)
+                .Verifiable();
+
+            mockBudgetaryRepository
+                .Setup(x => x.GetBudgetaryPoolsForUser(userId, true))
+                .Returns(pools)
+                .Verifiable();
+
+            mockEventProcessor
+                .Setup(x => x.ProcessEvents(budgetaryEvents, null, pools, It.IsAny<Action<BudgetaryEvent, FinancialState>>()))
+                .Returns(expectedFinancialState)
+                .Verifiable();
+
+            var uut = new FinancialStateService(mockFinancialSnapshotManager.Object, mockBudgetaryRepository.Object, mockEventProcessor.Object, mockDateTimeProvider.Object);
+
+            // When
+            var result = await uut.GetCurrentFinancialStateForUserAsync(userId);
+
+            // Then
+            Assert.That(result, Is.EqualTo(expectedFinancialState));
+
+            mockDateTimeProvider.Verify();
+            mockFinancialSnapshotManager.Verify(x => x.GetSnapshotAsync(userId), Times.Once);
+            mockBudgetaryRepository.Verify();
+            mockEventProcessor.Verify();
+        }
+    }
+}
--- Budgetty.Services/FinancialStateService.cs
+++ Budgetty.Services/FinancialStateService.cs
@@ -4,23 +4,25 @@ using Budgetty.Services.Interfaces;
 
 namespace Budgetty.Services
 {
-    internal class FinancialStateService : IFinancialStateService
+    public class FinancialStateService : IFinancialStateService
     {
         private readonly IFinancialsSnapshotManager _financialsSnapshotManager;
         private readonly IBudgetaryRepository _budgetaryRepository;
         private readonly IEventProcessor _eventProcessor;
+        private readonly IDateTimeProvider _dateTimeProvider;
 
-        public FinancialStateService(IFinancialsSnapshotManager financialsSnapshotManager, IBudgetaryRepository budgetaryRepository, IEventProcessor eventProcessor)
+        public FinancialStateService(IFinancialsSnapshotManager financialsSnapshotManager, IBudgetaryRepository budgetaryRepository, IEventProcessor eventProcessor, IDateTimeProvider dateTimeProvider)
         {
             _financialsSnapshotManager = financialsSnapshotManager;
             _budgetaryRepository = budgetaryRepository;
             _eventProcessor = eventProcessor;
+            _dateTimeProvider = dateTimeProvider;
         }
 
         public async Task<FinancialState> GetCurrentFinancialStateForUserAsync(string userId)
         {
             var financialsSnapshot = await _financialsSnapshotManager.GetSnapshotAsync(userId);
-            var now = DateOnly.FromDateTime(DateTime.UtcNow);
+            var now = _dateTimeProvider.GetDateNow();
             var allEvents = _budgetaryRepository.GetBudgetaryEventsForUser(userId, financialsSnapshot?.Date, now).ToList();
             var pools = _budgetaryRepository.GetBudgetaryPoolsForUser(userId, includeBankAccounts: true).ToList();
             var financialState = _eventProcessor.ProcessEvents(allEvents, financialsSnapshot, pools);
